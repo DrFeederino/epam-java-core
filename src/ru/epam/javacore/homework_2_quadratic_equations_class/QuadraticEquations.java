4a0a54e61d63c3e0ee777ad040a1b1392e67e7e8
DrFeederino <23013757+DrFeederino@users.noreply.github.com>
2019-12-05 18:07:13 +0300

    Fixes to homework 2

--- src/ru/epam/javacore/homework_2_quadratic_equations_class/QuadraticEquations.java
+++ src/ru/epam/javacore/homework_2_quadratic_equations_class/QuadraticEquations.java
@@ -2,30 +2,33 @@ package ru.epam.javacore.homework_2_quadratic_equations_class;
 
 public class QuadraticEquations {
     private double discriminant;
-    private float a;
-    private float b;
-    private float c;
+    private Coefficient a;
+    private Coefficient b;
+    private Coefficient c;
 
-    public QuadraticEquations(float a, float b, float c) {
+    public QuadraticEquations(Coefficient a, Coefficient b, Coefficient c) {
         this.a = a;
         this.b = b;
         this.c = c;
-        this.discriminant = solve();
     }
 
     public double solve() {
-        discriminant = Math.pow(b, 2) - 4 * a * c;
-
-        if (discriminant < 0) {
-            System.out.println("No real roots.");
-        } else if (discriminant == 0) {
-            double x = -b / (2 * a);
-            System.out.println("Root is " + x + ".");
+        if (a == null || b == null || c == null) {
+            System.out.println("One of the coefficient is set to null!");
         } else {
-            double x1 = (-b + Math.sqrt(this.discriminant)) / (2 * a);
-            double x2 = (-b - Math.sqrt(this.discriminant)) / (2 * a);
-            System.out.print("\nFirst root is " + x1);
-            System.out.println(", second root is " + x2 + ".");
+            discriminant = Math.pow(b.getValue(), 2) - 4 * a.getValue() * c.getValue();
+
+            if (discriminant < 0) {
+                System.out.println("No real roots.");
+            } else if (discriminant == 0) {
+                double x = -b.getValue() / (2 * a.getValue());
+                System.out.println("Root is " + x + ".");
+            } else {
+                double x1 = (-b.getValue() + Math.sqrt(this.discriminant)) / (2 * a.getValue());
+                double x2 = (-b.getValue() - Math.sqrt(this.discriminant)) / (2 * a.getValue());
+                System.out.print("\nFirst root is " + x1);
+                System.out.println(", second root is " + x2 + ".");
+            }
         }
         return discriminant;
     }
@@ -38,27 +41,27 @@ public class QuadraticEquations {
         this.discriminant = discriminant;
     }
 
-    public float getA() {
+    public Coefficient getA() {
         return a;
     }
 
-    public void setA(float a) {
+    public void setA(Coefficient a) {
         this.a = a;
     }
 
-    public float getB() {
+    public Coefficient getB() {
         return b;
     }
 
-    public void setB(float b) {
+    public void setB(Coefficient b) {
         this.b = b;
     }
 
-    public float getC() {
+    public Coefficient getC() {
         return c;
     }
 
-    public void setC(float c) {
+    public void setC(Coefficient c) {
         this.c = c;
     }
 
